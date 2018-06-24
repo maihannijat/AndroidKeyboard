8c142b8c1962c4a0ace74f257b05e21c496e2b33
Maihan Nijat <maihan.nijat84@stclairconnect.ca>
2018-06-23 20:40:14 -0400

    EditText, Button, Spinner and RecyclerView linked with ManagedActivity. Event Listener added. Search function. Array list for spinner items.

--- app/src/main/java/com/sunzala/afghankeyboard/DictionaryActivity.java
+++ app/src/main/java/com/sunzala/afghankeyboard/DictionaryActivity.java
@@ -2,12 +2,71 @@ package com.sunzala.afghankeyboard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class DictionaryActivity extends AppCompatActivity {
+import com.sunzala.afghankeyboard.database.DatabaseManager;
+
+import java.util.ArrayList;
+
+public class DictionaryActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText searchText;
+    Spinner languageSpinner;
+    ArrayList<String> languages;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dictionary);
+
+        languageSpinner = findViewById(R.id.spinner);
+        searchText = findViewById(R.id.searchText);
+        Button searchButton = findViewById(R.id.searchButton);
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+
+        // Set event listener on search button
+        searchButton.setOnClickListener(this);
+
+        // Array for spinner
+        languages = new ArrayList<>();
+        languages.add("English");
+        languages.add("Pashto");
+        languages.add("Persian");
+
+        // Add items to the spinner
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, languages);
+        languageSpinner.setAdapter(adapter);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.searchButton:
+                search();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void search() {
+        String searchWord = searchText.getText().toString();
+
+        if(!searchWord.equals("")) {
+            DatabaseManager db = new DatabaseManager(this);
+
+            String language = languages.get(languageSpinner.getSelectedItemPosition());
+        } else {
+            Toast.makeText(this, getString(R.string.search_hint_toast), Toast.LENGTH_LONG).show();
+        }
     }
 }
