1dc0b714f646e0490cef221e3fd5f955def515f3
Maihan Nijat <maihan.nijat84@stclairconnect.ca>
2018-06-23 23:57:15 -0400

    Calls delete function on DatabaseManager and delete the word. Update the adapter to refresh the list.

--- app/src/main/java/com/sunzala/afghankeyboard/adapter/SearchAdapter.java
+++ app/src/main/java/com/sunzala/afghankeyboard/adapter/SearchAdapter.java
@@ -1,14 +1,25 @@
 package com.sunzala.afghankeyboard.adapter;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.sunzala.afghankeyboard.R;
+import com.sunzala.afghankeyboard.database.DatabaseManager;
+
 import java.util.ArrayList;
 
 public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.ViewHolder> {
     private ArrayList<String> mDataset;
+    private Context context;
+    private String subType;
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
@@ -16,15 +27,20 @@ public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.ViewHolder
     public static class ViewHolder extends RecyclerView.ViewHolder {
         // each data item is just a string in this case
         public TextView mTextView;
-        public ViewHolder(TextView v) {
+        public Button button;
+
+        public ViewHolder(View v) {
             super(v);
-            mTextView = v;
+            mTextView = v.findViewById(R.id.textView);
+            button = v.findViewById(R.id.button);
         }
     }
 
     // Provide a suitable constructor (depends on the kind of dataset)
-    public SearchAdapter(ArrayList<String> myDataset) {
+    public SearchAdapter(ArrayList<String> myDataset, Context context, String subType) {
         mDataset = myDataset;
+        this.context = context;
+        this.subType = subType;
     }
 
     // Create new views (invoked by the layout manager)
@@ -32,10 +48,9 @@ public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.ViewHolder
     public SearchAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
                                                    int viewType) {
         // create a new view
-        TextView v = (TextView) LayoutInflater.from(parent.getContext())
-                .inflate(android.R.layout.simple_list_item_1, parent, false);
-        ViewHolder vh = new ViewHolder(v);
-        return vh;
+        View v = (View) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.search_words_list, parent, false);
+        return new ViewHolder(v);
     }
 
     // Replace the contents of a view (invoked by the layout manager)
@@ -44,7 +59,18 @@ public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.ViewHolder
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
         holder.mTextView.setText(mDataset.get(position));
+        holder.button.setOnClickListener((View v) -> {
+            DatabaseManager db = new DatabaseManager(context);
+            try {
+                db.delete(mDataset.get(position), subType);
+            } catch (Exception e) {
+                Log.d("Exception Error", String.valueOf(e));
+            }
 
+            mDataset.remove(position);
+            this.notifyDataSetChanged();
+            Toast.makeText(context, R.string.removed_word, Toast.LENGTH_LONG).show();
+        });
     }
 
     // Return the size of your dataset (invoked by the layout manager)
